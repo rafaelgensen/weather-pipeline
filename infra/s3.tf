28b51c9b8c3821ad98600ddc2818d2ad47b818a7
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-09-15 10:41:24 -0300

    fix: s3 buckets policies + CI

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,3 +1,4 @@
+# Buckets
 resource "aws_s3_bucket" "backend" {
   bucket = "weather-states-663354324751"
 }
@@ -14,36 +15,59 @@ resource "aws_s3_bucket" "glue_scripts" {
   bucket = "weather-glue-scripts-663354324751"
 }
 
+# IAM Role para Glue
+resource "aws_iam_role" "glue_role" {
+  name = "factored-glue-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Principal = { Service = "glue.amazonaws.com" }
+      Effect = "Allow"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "glue_s3" {
+  role       = aws_iam_role.glue_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+# Glue Service Role policy removida (não existe attachable)
+# resource "aws_iam_role_policy_attachment" "glue_catalog" {
+#   role       = aws_iam_role.glue_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AWSGlueServiceRole"
+# }
+
+# Bucket Policies corretas
 data "aws_iam_policy_document" "glue_bucket_access" {
   statement {
     effect = "Allow"
-
     principals {
       type        = "Service"
       identifiers = ["glue.amazonaws.com"]
     }
-
     actions = [
       "s3:GetObject",
       "s3:PutObject",
       "s3:DeleteObject",
       "s3:ListBucket"
     ]
-
     resources = [
-      aws_s3_bucket.backend.arn,
-      "${aws_s3_bucket.backend.arn}/*",
-      aws_s3_bucket.raw.arn,
-      "${aws_s3_bucket.raw.arn}/*",
-      aws_s3_bucket.processed.arn,
-      "${aws_s3_bucket.processed.arn}/*",
-      aws_s3_bucket.glue_scripts.arn,
-      "${aws_s3_bucket.glue_scripts.arn}/*"
+      "arn:aws:s3:::${aws_s3_bucket.backend.bucket}",
+      "arn:aws:s3:::${aws_s3_bucket.backend.bucket}/*",
+      "arn:aws:s3:::${aws_s3_bucket.raw.bucket}",
+      "arn:aws:s3:::${aws_s3_bucket.raw.bucket}/*",
+      "arn:aws:s3:::${aws_s3_bucket.processed.bucket}",
+      "arn:aws:s3:::${aws_s3_bucket.processed.bucket}/*",
+      "arn:aws:s3:::${aws_s3_bucket.glue_scripts.bucket}",
+      "arn:aws:s3:::${aws_s3_bucket.glue_scripts.bucket}/*"
     ]
   }
 }
 
-resource "aws_s3_bucket_policy" "glue_access" {
+resource "aws_s3_bucket_policy" "glue_access_backend" {
   bucket = aws_s3_bucket.backend.id
   policy = data.aws_iam_policy_document.glue_bucket_access.json
 }
