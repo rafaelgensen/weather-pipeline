0368e194c0414c6213e697bc3ea16346c8cb1294
Rafael Gensen <rafaelggcaldeira@gmail.com>
2025-09-15 10:46:58 -0300

    fix: s3 buckets policies + CI

--- infra/iam.tf
+++ infra/iam.tf
@@ -17,8 +17,3 @@ resource "aws_iam_role_policy_attachment" "glue_s3" {
   role       = aws_iam_role.glue_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
-
-resource "aws_iam_role_policy_attachment" "glue_catalog" {
-  role       = aws_iam_role.glue_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSGlueServiceRole"
-}
